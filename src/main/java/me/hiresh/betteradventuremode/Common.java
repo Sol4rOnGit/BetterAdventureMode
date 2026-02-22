c7800c6497c914238f6950d260ad30be90d8c297
Sol4rOnGit <Hiresh.Solanki@outlook.com>
2026-02-22 13:34:50 +0000

    Break non tool-specific blocks by hand (1.1)
    This includes flowers, etc. which are impossible to break otherwise.
    
    v1.1

--- src/main/java/me/hiresh/betteradventuremode/Common.java
+++ src/main/java/me/hiresh/betteradventuremode/Common.java
@@ -1,6 +1,10 @@
 package me.hiresh.betteradventuremode;
 
+//Events
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.event.player.
+
+//Minecraft Imports
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.text.Text;
@@ -10,6 +14,12 @@ public class Common {
         PlayerBlockBreakEvents.BEFORE.register((world, playerEntity, blockPos, blockState, blockEntity) -> {
             ItemStack heldItem = playerEntity.getMainHandStack();
 
+            if (!           (blockState.isIn(BlockTags.AXE_MINEABLE)
+                            || blockState.isIn(BlockTags.PICKAXE_MINEABLE)
+                            || blockState.isIn(BlockTags.SHOVEL_MINEABLE)
+                            || blockState.isIn(BlockTags.HOE_MINEABLE)))
+            { return true; }
+
             if (!heldItem.isSuitableFor(blockState)){
                 playerEntity.sendMessage(Text.literal("You need the correct tool!"), true);
                 return false;
@@ -17,5 +27,6 @@ public class Common {
 
             return true;
         });
+
     }
 }
