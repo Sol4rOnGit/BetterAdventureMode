2a76ca2fe73c959a4b8b0c61012e16c0fb3fcded
Sol4rOnGit <Hiresh.Solanki@outlook.com>
2026-02-22 14:18:46 +0000

    Slower Mining (v2)
    Enabled Slower Mining
    
    [Note: This may be broken because I forgot to commit this first. If this doesn't work, v3 will work]

---
+++ src/main/java/me/hiresh/betteradventuremode/mixin/PlayerEntityMixin.java
@@ -0,0 +1,36 @@
+package me.hiresh.betteradventuremode.mixin;
+
+//Mixin Imports
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+//Minecraft Imports
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.BlockTags;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+
+    static {
+        System.out.println("[BetterAdventureMode] PlayerEntityMixin loaded!");
+    }
+
+    @ModifyReturnValue(method = "getBlockBreakingSpeed", at = @At("RETURN"))
+    private float modifyBreakSpeed(float originalSpeed, BlockState blockState){
+
+        boolean requiresTool = (blockState.isIn(BlockTags.AXE_MINEABLE)
+                || blockState.isIn(BlockTags.PICKAXE_MINEABLE)
+                || blockState.isIn(BlockTags.SHOVEL_MINEABLE)
+                || blockState.isIn(BlockTags.HOE_MINEABLE));
+
+        if (requiresTool){
+            return  originalSpeed / 10f; //slower (Change the number to change divisor)
+        }
+
+        return originalSpeed; //instant blocks mine normal speed
+    }
+}
