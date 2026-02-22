0022d2e3a0dcceec9d52fb5102b458f5f92ba7cf
Sol4rOnGit <Hiresh.Solanki@outlook.com>
2026-02-22 17:10:40 +0000

    Public Builds + Removed Ores (v3.1)
    Removed the slow mining for ores.
    
    Additionally removed .jar from git ignore so that I can include the Builds

--- src/main/java/me/hiresh/betteradventuremode/mixin/PlayerEntityMixin.java
+++ src/main/java/me/hiresh/betteradventuremode/mixin/PlayerEntityMixin.java
@@ -2,6 +2,7 @@ package me.hiresh.betteradventuremode.mixin;
 
 //Mixin Imports
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -22,12 +23,21 @@ public class PlayerEntityMixin {
     @ModifyReturnValue(method = "getBlockBreakingSpeed", at = @At("RETURN"))
     private float modifyBreakSpeed(float originalSpeed, BlockState blockState){
 
-        boolean requiresTool = (blockState.isIn(BlockTags.AXE_MINEABLE)
+        boolean applicable =
+                //All of the blocks that require Tools
+                (blockState.isIn(BlockTags.AXE_MINEABLE)
                 || blockState.isIn(BlockTags.PICKAXE_MINEABLE)
                 || blockState.isIn(BlockTags.SHOVEL_MINEABLE)
-                || blockState.isIn(BlockTags.HOE_MINEABLE));
+                || blockState.isIn(BlockTags.HOE_MINEABLE))
+                //Exclude Ores
+                && !(blockState.isIn(BlockTags.COAL_ORES)
+                || blockState.isIn(BlockTags.DIAMOND_ORES)
+                || blockState.isIn(BlockTags.GOLD_ORES)
+                || blockState.isIn(BlockTags.IRON_ORES)
+                || blockState.isIn(BlockTags.REDSTONE_ORES)
+                || blockState.isIn(BlockTags.LAPIS_ORES));
 
-        if (requiresTool){
+        if (applicable){
             return  originalSpeed / 10f; //slower (Change the number to change divisor)
         }
 
